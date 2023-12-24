dd2431d4f316206b2bcf0aefb471a3df4e7df98d
devmagda <113769198+devmagda@users.noreply.github.com>
2023-12-24 13:09:41 +0100

    Updated debug copy script

--- src/lab/pi/copy_to_pi.sh
+++ src/lab/pi/copy_to_pi.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 scp *.py dev@pi.local:/home/dev/laboratory/electronics
+scp static/* dev@pi.local:/home/dev/laboratory/electronics/static
+scp templates/* dev@pi.local:/home/dev/laboratory/electronics/templates
